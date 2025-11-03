1fc2eb8f0ed08be45732a599c429ba66179b0b98
Oleh_Hulevych <oleggulevic8@gmail.com>
2025-11-03 20:59:49 +0100

    backend is created and User model is added

---
+++ server/models/User.php
@@ -0,0 +1,65 @@
+<?php
+class User {
+    private $file = __DIR__ . "/../data/users.json";
+
+    private function getData(){
+        if(file_exists($this->file)){
+            file_put_contents($this->file,[]);
+        }
+        $items = file_get_contents($this->file);
+        return json_decode($items,true);
+    }
+    private function saveData ($data)
+    {
+        file_put_contents($this->file, json_encode($data,JSON_PRETTY_PRINT));
+        echo "The data is saved";
+    }
+
+    public function getAll(){
+       return $this->file;
+
+    }
+    public function getUserById($id){
+        $users = $this->getData();
+        foreach ($users as $user){
+            if($user['id']==$id){
+                return $user;
+            }
+        }
+        return null;
+
+    }
+
+    public function create($name, $email, $photoPath, $phone){
+        $users = $this->getData();
+        $newid = count($users) ? end($users)['id']+1:1;
+        $newUser = ["id"=>$newid, "name"=>$name, "email"=>$email, "photoPath" => $photoPath, "phone"=>$phone ];
+        $items[] = $newUser;
+        $this->saveData($items);
+        return $newUser;
+    }
+
+    public function update($id,$name, $email, $photoPath, $phone){
+        $users = $this->getData();
+        foreach ($users as $user){
+             if($user==$id){
+                 $user['name'] = $name;
+                 $user['email'] = $email;
+                 $user['photoPath'] = $photoPath;
+                 $user['phone'] = $phone;
+                 $this->saveData($users);
+                 return $user;
+             }
+        }
+        return null;
+    }
+    public function delete($id){
+        $users = $this->getData();
+        $filteredUsers = array_filter($users, fn($user)=>$user['id'] != $id);
+        $this->saveData($filteredUsers);
+        return true;
+    }
+
+
+
+}
