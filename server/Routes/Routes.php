ad2a57930803b6277d6774ea85fffe11de232dbd
Oleh_Hulevych <oleggulevic8@gmail.com>
2025-11-14 10:52:49 +0100

    settings for apache was added

--- server/Routes/Routes.php
+++ server/Routes/Routes.php
@@ -3,6 +3,7 @@ require_once __DIR__ . "/../controllers/UserController.php";
 require_once  __DIR__ ."/../controllers/ProductController.php";
 
 $uri  = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = str_replace('/server', '', $uri);
 $method = $_SERVER['REQUEST_METHOD'];
 $userController = new UserController();
 $productController = new ProductController();
