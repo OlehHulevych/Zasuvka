5e3e991b923fd9c9cee0cdefff1a015a01f0cc5a
Oleh_Hulevych <oleggulevic8@gmail.com>
2025-11-18 17:49:42 +0100

    connected frontend and backend

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,3 +1,4 @@
+import {config} from "./config.js";
 //mobile search bar functionality
 let mobileSearchBarButton = document.getElementById('mobile_search_button');
 mobileSearchBarButton.addEventListener('click', ()=>{
@@ -9,4 +10,24 @@ document.getElementById('cancel_button').addEventListener('click',(e)=>{
     e.preventDefault();
     let mobileSearchBar = document.getElementById('mobile_search_bar')
     mobileSearchBar.classList.remove('active')
+})
+
+document.addEventListener("DOMContentLoaded",async()=>{
+    const response = await fetch(config.API_URL + "/product",{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        credentials:"include"
+    });
+
+    const data = await response.json();
+    //console.log(data)
+    console.log(data)
+    let container = document.getElementById("ad_container");
+    let productHtml = '';
+    
+    container.innerHTML = data.products.items.map(item=>{
+
+    })
 })
