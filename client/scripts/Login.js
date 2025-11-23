c3f5d262d5965c90e3b30e8920f9d54a6c087abf
Oleh_Hulevych <oleggulevic8@gmail.com>
2025-11-23 13:18:50 +0100

    login is implemented

---
+++ client/scripts/Login.js
@@ -0,0 +1,33 @@
+import {config} from "./config.js";
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    e.preventDefault();
+    const loginForm = document.getElementById("login_form");
+    loginForm.addEventListener("submit", async(e)=>{
+        e.preventDefault();
+        const formData = new FormData(loginForm)
+        try{
+            const response = await fetch(config.API_URL+"/user/login",{
+                method:"POST",
+                body:formData,
+                credentials:"include"
+            })
+            if(response.ok){
+                const data = await response.json();
+                console.log(data)
+                const userId = data.user_id;
+                sessionStorage.setItem("user_id", userId);
+                console.log(sessionStorage.getItem("user_id"));
+                window.location.href = "/Zasuvka/client"
+            }
+            else{
+                const data = await response.json;
+                console.log(data.message);
+            }
+        }
+        catch (error){
+            console.log(error);
+        }
+
+    })
+})
--- client/scripts/Registration.js
+++ client/scripts/Registration.js
@@ -7,32 +7,37 @@ document.addEventListener("DOMContentLoaded",(e)=>{
     reg_form.addEventListener("submit", async(e)=>{
         e.preventDefault();
         const formData  = new FormData(reg_form);
+        const data = Object.fromEntries(formData)
+        console.log(data);
         if(!formData.name || !formData.email || !formData.phone || !formData.password || !formData){
             warning_block.classList.add("active")
             warning_block.innerHTML = "Folmulář neni vyplněný"
         }
-        if(!formData.checkbox.checked){
+        if(!formData.checkbox !== "on"){
             warning_block.classList.add("active")
             warning_block.innerHTML = "Nesouhlasili jste s našimi podminkami"
         }
-        const data = Object.fromEntries(formData)
+
         console.log(data)
         try{
-            const response = await fetch(config.API_URL + "/user/registration", {
+            const response = await fetch(config.API_URL + "/user/register", {
                 method:'POST',
-                body:JSON.stringify(data)
+                body:formData,
+                credentials:"include"
             })
             if(response.ok){
                 warning_block.classList.remove("active")
                 const result = await response.json();
                 console.log('Success', result);
+                window.location.href = "/Zasuvka/Client/login.html"
             }
             else{
-                console.error('Server Error', response.status)
+                const result = await response.json();
+                console.log(result)
             }
         }
         catch (error){
-            console.error("Network Error", error)
+            console.log(error)
         }
 
     })
